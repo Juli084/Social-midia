a485bb64f06908198f957da70e6f78f52178b88d
Juli084 <juliorodrigues84@live.com>
2024-04-22 20:18:04 -0300

    estrutura inicial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Post } from "./assets/Post"
+
+function App() {
+  return (
+   <div>
+      <Post
+        author="Júlio Rodrigues"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic minima nihil quo vitae, eligendi ullam nulla voluptatibus deserunt dignissimos blanditiis consequatur praesentium minus doloremque quisquam enim sed. Quaerat, dolores nihil!"
+      />
+      <Post
+      author="Francesco Terezo"
+      content="Que legal!"
+      />    
+   </div>
+  )
+}
+
+export default App
+
