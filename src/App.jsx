830eee708376c7d651a5a057728793512185d852
Juli084 <juliorodrigues84@live.com>
2024-04-22 22:01:08 -0300

    Commit: Estrutura da aplicação - Componente: Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { Post } from "./assets/Post"
+import { Post } from "./Post"
+import { Header } from "./components/Header";
+
+import './global.css';
 
 function App() {
   return (
    <div>
+
+      <Header/>
       <Post
         author="Júlio Rodrigues"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic minima nihil quo vitae, eligendi ullam nulla voluptatibus deserunt dignissimos blanditiis consequatur praesentium minus doloremque quisquam enim sed. Quaerat, dolores nihil!"
